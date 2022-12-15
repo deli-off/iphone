532190896bd9170257bda2ade82115cf6ef4c5cf
deli-off <dzamsedovnazif1@gmail.com>
2022-12-15 22:07:50 +0500

    finished

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+let blue_btn = document.querySelector('.blue')
+let purple_btn = document.querySelector('.purple')
+let midnight_btn = document.querySelector('.starlight')
+let red_btn = document.querySelector('.red')
 
+blue_btn.onclick = () => {
+
+}
 
 
 
