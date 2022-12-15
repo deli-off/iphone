e6751fda9635771331a42e97c956edeabd17471e
deli-off <dzamsedovnazif1@gmail.com>
2022-12-15 23:01:28 +0500

    finished

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ let blue_btn = document.querySelector('.blue')
 let purple_btn = document.querySelector('.purple')
 let midnight_btn = document.querySelector('.starlight')
 let red_btn = document.querySelector('.red')
+let img = document.querySelector('.scroll-img')
 
-blue_btn.onclick = () => {
-
+purple_btn.onclick = () => {
+    if (purple_btn.classList.contains('purple')) {
+        img.classList.remove('blue')
+        img.classList.add('purple__img')
+    } else {
+        img.classList.add('.purple__img')
+    }
 }
 
 
