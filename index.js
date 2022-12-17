d26abf6e9145c613f2e4d6ae3d81460bccb05e98
deli-off <dzamsedovnazif1@gmail.com>
2022-12-17 22:21:20 +0500

    finished

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-let blue_btn = document.querySelector('.blue')
-let purple_btn = document.querySelector('.purple')
-let midnight_btn = document.querySelector('.starlight')
-let red_btn = document.querySelector('.red')
-let img = document.querySelector('.scroll-img')
+let blue = document.querySelector('.blue');
+let purple = document.querySelector('.purple');
+let midnight = document.querySelector('.midnight');
+let starlight = document.querySelector('.starlight');
+let red = document.querySelector('.red');
+let img = document.querySelector('.scroll-section__inner-img');
 
-purple_btn.onclick = () => {
-    if (purple_btn.classList.contains('purple')) {
-        img.classList.remove('blue')
-        img.classList.add('purple__img')
-    } else {
-        img.classList.add('.purple__img')
-    }
+blue.onclick = () => {
+    img.style.backgroundImage = "url('./img/blue-iphone.jpg')"
 }
+purple.onclick = () => {
+    img.style.backgroundImage = "url('./img/purple-iphone.jpg')"
+}
+midnight.onclick = () => {
+    img.style.backgroundImage = "url('./img/midknight-iphone.jpg')"
+}
+starlight.onclick = () => {
+    img.style.backgroundImage = "url('./img/starlight-iphone.jpg')"
+}
+red.onclick = () => {
+    img.style.backgroundImage = "url('./img/red-iphone.jpg')"
+}
+
+// 2 способ
+
+// let allBtns = document.querySelectorAll('button');
+// let img = document.querySelector('.scroll-section__inner-img');
+
+
+// allBtns.forEach(btn => {
+//     btn.onclick = () => {
+//         if (btn.classList === 'blue') {
+//             img.style.backgroundImage = "url('./img/blue-iphone.jpg')"
+//         } else if (btn.classList === 'purple') {
+//             img.style.backgroundImage = "url('./img/purple-iphone.jpg')"
+//         } else if (btn.classList === 'midnight') {
+//             img.style.backgroundImage = "url('./img/midknihgt-iphone.jpg')"
+//         } else if (btn.classList === 'starlight') {
+//             img.style.backgroundImage = "url('./img/starlight-iphone.jpg')"
+//         } else if (btn.classList === 'red') {
+//             img.style.backgroundImage = "url('./img/red-iphone.jpg')"
+//         }
+//     }
+// })
+
 
 
 
